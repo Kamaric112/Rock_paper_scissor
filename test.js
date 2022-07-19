73f7e817e9d8140052cab373bbce7e249266459a
Kamaric112 <ikasuzu123@gmail.com>
2022-07-19 21:04:14 +0700

    updated playRound function logic ( UI update WIP)

--- test.js
+++ test.js
@@ -11,23 +11,23 @@ let computerScore = 0
 
 const rocks = document.querySelector('#rock')
 rocks.addEventListener('click',function(e) {
-    playerSelection = "rock";
+    playerSelection = this.id
     console.log(playerSelection)
     playRound(playerSelection,computerPlay())
-    e.stopPropagation();
+    e.stopPropagation()
 
 })
 
 const papers = document.querySelector('#paper')
 papers.addEventListener('click',function() {
-    playerSelection = "paper";
+    playerSelection = this.id
     console.log(playerSelection)
     playRound(playerSelection,computerPlay())
 })
 
 const scissor = document.querySelector('#scissors')
 scissor.addEventListener('click',function() {
-    playerSelection = "scissors";
+    playerSelection = this.id
     console.log(playerSelection)
     playRound(playerSelection,computerPlay())
 })
@@ -40,114 +40,57 @@ function disableButtons() {
     })
 }
 
-const container = document.querySelector('#container');
+const container = document.querySelector('#container')
 
-const div =document.createElement('div');
-div.classList.add('result');
+const div =document.createElement('div')
+div.classList.add('result')
 
 const player = document.createElement('div')
 player.classList.add("pscore")
 
-
 const comp = document.createElement('div')
 comp.classList.add("cscore")
 
+
+
+
+// player choose rock and computer choose paper OR player choose paper and computer choose scissors OR player choose scissors and computer choose rock ==> computer WIN
+
+// player choose rock and computer choose scissors OR player choose paper and computer choose rock OR player choose scissors and computer choose paper ==> player WIN
+
+// player choose rock and computer choose rock OR player choose paper and computer choose rock OR player choose scissors and computer choose scissors ==> TIE
+
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay()
-    if (playerSelection == "rock") {
-        if (computerSelection =="rock") {
-            console.log( "tie" )
-            div.textContent = "tie"
-            container.appendChild(div);
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
+    if (playerSelection == "rock" && computerSelection =="scissors" || playerSelection == "paper" && computerSelection =="rock"||playerSelection == "scissors" && computerSelection =="paper") { // player wins
+        div.textContent = `${playerSelection} beats ${computerSelection}` 
+        container.appendChild(div);
+        playerScore++
+        player.textContent = `player score is ${playerScore}`
+        comp.textContent = `computer score is ${computerScore}`
+        container.appendChild(player)
+        container.appendChild(comp)
+       }
 
-        }
-        else if (computerSelection == "scissors") {
-            console.log ( `${playerSelection} beats ${computerSelection}` )
-            div.textContent = `${playerSelection} beats ${computerSelection}` 
-            container.appendChild(div);
-            playerScore++
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
-        else if ((computerSelection == "paper")) {
-            console.log( `${computerSelection} beats ${playerSelection}` )
-            div.textContent = `${computerSelection} beats ${playerSelection}`
-            container.appendChild(div);
-            computerScore++
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
+    else if ( playerSelection == "rock" && computerSelection == "paper" || playerSelection =="paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection =="rock") { // computer wins
+        div.textContent = `${computerSelection} beats ${playerSelection}`
+        container.appendChild(div);
+        computerScore++
+        player.textContent = `player score is ${playerScore}`
+        comp.textContent = `computer score is ${computerScore}`
+        container.appendChild(player)
+        container.appendChild(comp)
     }
 
-    if (playerSelection == "paper") {
-        if (computerSelection =="paper") {
-            div.textContent = "tie"
-            container.appendChild(div);
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
-        else if (computerSelection == "rock") {
-            console.log( `${playerSelection} beats ${computerSelection}` )
-            div.textContent = `${playerSelection} beats ${computerSelection}` 
-            container.appendChild(div);
-            playerScore++
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
-        else if ((computerSelection == "scissors")) {
-            console.log( `${computerSelection} beats ${playerSelection}` )
-            div.textContent = `${computerSelection} beats ${playerSelection}`
-            container.appendChild(div);
-            computerScore++
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
+    else if ( playerSelection == "rock" && computerSelection == "rock" || playerSelection =="paper" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection =="scissors") { // tie
+        div.textContent = "tie"
+        container.appendChild(div);
+        player.textContent = `player score is ${playerScore}`
+        comp.textContent = `computer score is ${computerScore}`
+        container.appendChild(player)
+        container.appendChild(comp)
     }
 
-    if (playerSelection == "scissors") {
-        if (computerSelection =="scissors") {
-            div.textContent = "tie"
-            container.appendChild(div);
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
-        else if (computerSelection == "paper") {
-            console.log( `${playerSelection} beats ${computerSelection}` )
-            div.textContent = `${playerSelection} beats ${computerSelection}` 
-            container.appendChild(div);
-            playerScore++
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)        }
-        else if ((computerSelection == "rock")) {
-            console.log( `${computerSelection} beats ${playerSelection}` )
-            div.textContent = `${computerSelection} beats ${playerSelection}`
-            container.appendChild(div);
-            computerScore++
-            player.textContent = `${playerScore}`
-            comp.textContent = `${computerScore}`
-            container.appendChild(player)
-            container.appendChild(comp)
-        }
-    }
-    
         if (playerScore === 5 ) {
             alert(`${playerSelection} (You) won!`)
             disableButtons()
